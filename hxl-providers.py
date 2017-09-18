05ddb54b83c61f2fb3d961fd170e7f30fdaa1329
David Megginson <david.megginson@megginson.com>
2017-09-18 18:23:23 -0400

    Building dataset correctly.

--- hxl-providers.py
+++ hxl-providers.py
@@ -5,10 +5,10 @@ Python CKAN library: https://github.com/ckan/ckanapi
 Started by David Megginson, 2017-09-17
 """
 
-import ckanapi, time
+import ckanapi, time, sys
 from pprint import pprint
 
-DELAY = 5
+DELAY = 2
 """Time delay in seconds between datasets, to give HDX a break."""
 
 CHUNK_SIZE=100
@@ -16,6 +16,9 @@ CHUNK_SIZE=100
 
 CKAN_URL = 'https://data.humdata.org'
 """Base URL for the CKAN instance."""
+
+hxl_providers = {}
+"""List of HXL data providers, the number of datasets, and the earliest date."""
 
 # Open a connection to HDX
 ckan = ckanapi.RemoteCKAN(CKAN_URL)
@@ -26,8 +29,26 @@ result_count = 999999
 while start < result_count:
     result = ckan.action.package_search(fq='tags:hxl', start=start, rows=CHUNK_SIZE)
     result_count = result['count']
-    print(len(result['results']))
+    print("Read {} package(s)...".format(len(result['results'])), file=sys.stderr)
+    for package in result['results']:
+        org = package['organization']
+        date = package['metadata_created']
+        record = hxl_providers.get(org['name'])
+        if record:
+            record['count'] += 1
+            if record['date'] > date:
+                record['date'] = date
+        else:
+            record = {
+                'name': org['name'],
+                'title': org['title'],
+                'date': date,
+                'count': 1
+            }
+        hxl_providers[org['name']] = record
     start += CHUNK_SIZE
     time.sleep(DELAY) # give HDX a short rest
+
+pprint(hxl_providers)
 
 # end
