02fa2ea9ef509c7d7c5bd5e2f2186f6eed2e3619
David Megginson <david.megginson@gmail.com>
2021-02-04 16:27:54 -0500

    Update hxl-providers script to use ckancrawler (also fixes bug that cause 2 providers to be ignored).

--- hxl-providers.py
+++ hxl-providers.py
@@ -6,50 +6,53 @@ Started by David Megginson, 2017-09-17
 """
 
 import config # standard configuration
-import ckanapi, time, sys, csv
+import ckancrawler, csv, logging, sys
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger("hxl-providers")
+"""Set up a logger"""
 
 hxl_providers = {}
 """List of HXL data providers, the number of datasets, and the earliest date."""
 
 # Open a connection to HDX
-ckan = ckanapi.RemoteCKAN(config.CKAN_URL)
+crawler = ckancrawler.Crawler(config.CKAN_URL, delay=0, user_agent=config.USER_AGENT)
 
+# Iterate through all the datasets ("packages") and resources on HDX
+for i, package in enumerate(crawler.packages(fq=config.SEARCH_FQ, sort="metadata_created desc")):
+    org = package['organization']
+    created_date = package['metadata_created'][:10]
+    modified_date = package['metadata_modified'][:10]
+    record = hxl_providers.get(org['name'])
+    if record:
+        # If we've already seen this, update the count and check for earlier data
+        record['count'] += 1
+        if record['first_shared_date'] > created_date:
+            record['first_shared_date'] = created_date
+        if record['last_updated_date'] < modified_date:
+            record['last_updated_date'] = modified_date
+    else:
+        # If we haven't seen this yet, create the record
+        record = {
+            'name': org['name'],
+            'title': org['title'],
+            'first_shared_date': created_date,
+            'last_updated_date': modified_date,
+            'count': 1
+        }
+    hxl_providers[org['name']] = record # update the record
+
+    # let the user know how it's going
+    if (i+1) % 100 == 0:
+        logger.info("Processed %d datasets, %d unique orgs...", i+1, len(hxl_providers))
+else:
+    # print the final results
+    logger.info("Total: %d datasets, %d unique orgs.", i+1, len(hxl_providers))
+
+# Print the output to CSV on STDOUT
 # Open a CSV output stream
 output = csv.writer(sys.stdout)
 
-# Iterate through all the datasets ("packages") and resources on HDX
-start = 0
-result_count = 999999 # just a big, big number; will reset on first search result
-while start < result_count:
-    result = ckan.action.package_search(fq=config.SEARCH_FQ, start=start, rows=config.CHUNK_SIZE)
-    result_count = result['count']
-    print("Read {} package(s)...".format(len(result['results'])), file=sys.stderr)
-    for package in result['results']:
-        org = package['organization']
-        created_date = package['metadata_created'][:10]
-        modified_date = package['metadata_modified'][:10]
-        record = hxl_providers.get(org['name'])
-        if record:
-            # If we've already seen this, update the count and check for earlier data
-            record['count'] += 1
-            if record['first_shared_date'] > created_date:
-                record['first_shared_date'] = created_date
-            if record['last_updated_date'] < modified_date:
-                record['last_updated_date'] = modified_date
-        else:
-            # If we haven't seen this yet, create the record
-            record = {
-                'name': org['name'],
-                'title': org['title'],
-                'first_shared_date': created_date,
-                'last_updated_date': modified_date,
-                'count': 1
-            }
-        hxl_providers[org['name']] = record # update the record
-    start += config.CHUNK_SIZE # next chunk, but first ...
-    time.sleep(config.DELAY) # give HDX a short rest
-
-# Print the output to CSV on STDOUT
 output.writerow([
     'HDX org',
     'Org title',
